380a011e1da9e69cb2b0dfdae47cdafa165f1343
FreshManzzzzz <1027118385@qq.com>
2022-02-17 17:20:23 +0800

    完成app和Model代码的创建

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'config',
+    'comment',
+
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
