949f3680bb49d3d8b7fe79018f1a800d322162b1
FreshManzzzzz <1027118385@qq.com>
2022-02-18 21:05:12 +0800

    6.1 配置基础的admin页面

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -20,6 +20,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '分类'
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     STATUS_NORMAL = 1
@@ -36,7 +39,10 @@ class Tag(models.Model):
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
-        verbose_name = verbose_name_plural = '分类'
+        verbose_name = verbose_name_plural = '标签'
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -62,3 +68,6 @@ class Post(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '文章'
         ordering = ['-id']  # 根据ID进行降序排列
+
+    def __str__(self):
+        return self.title
