a8d1f9ae134bdf6999f343f73b19769edff35ee0
FreshManzzzzz <1027118385@qq.com>
2022-02-25 14:54:54 +0800

    7.4 整理模板代码

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -20,11 +20,11 @@ from blog.views import post_list, post_detail
 from config.views import links
 
 urlpatterns = [
-    url(r'^$', post_list),
-    url(r'^category/(?P<category_id>\d+)/$', post_list),
-    url(r'^tag/(?P<tag_id>\d+)/$', post_list),
-    url(r'^post/(?P<post_id>\d+).html', post_detail),
-    url(r'^links/$', links),
-    url(r'^super_admin/', admin.site.urls),
-    url(r'^admin/', custom_site.urls),
+    url(r'^$', post_list, name='index'),
+    url(r'^category/(?P<category_id>\d+)/$', post_list, name='category-list'),
+    url(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
+    url(r'^post/(?P<post_id>\d+).html', post_detail, name='post-detail'),
+    url(r'^links/$', links, name='links'),
+    url(r'^super_admin/', admin.site.urls, name='super-admin'),
+    url(r'^admin/', custom_site.urls, name='admin'),
 ]
