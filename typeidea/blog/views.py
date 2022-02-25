b0f4752ca060a82646d25b47385a2e3d58584de7
FreshManzzzzz <1027118385@qq.com>
2022-02-25 17:21:43 +0800

    7.5 class-based view

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,55 +1,117 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from blog.models import Post, Tag, Category
 from config.models import SideBar
+from django.views.generic import ListView, DetailView
 
 
-def post_list(request, category_id=None, tag_id=None):
-    tag = None
-    category = None
-    #
-    # if tag_id:
-    #     try:
-    #         tag = Tag.objects.get(id=tag_id)
-    #     except Tag.DoseNotExist:
-    #         post_list = []
-    #     else:
-    #         post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
-    # else:
-    #     post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
-    #     if category_id:
-    #         try:
-    #             category = Category.objects.get(id=category_id)
-    #         except Category.DoesNotExist:
-    #             category = None
-    #         else:
-    #             post_list = post_list.filter(category_id=category_id)
-    if tag_id:
-        post_list, tag = Post.get_by_tag(tag_id)
-    elif category_id:
-
-        post_list, category = Post.get_by_category(category_id)
-    else:
-        post_list = Post.latest_posts()
-
-    context = {
-        'tag': tag,
-        'category': category,
-        'post_list': post_list,
-        'sidebars': SideBar.get_all(),
-    }
-    context.update(Category.get_navs())
-
-    return render(request, 'blog/list.html', context=context)
+# 将function view改造为class-based view
+# 专门用来处理页面的通用数据，如分类数据，侧边栏数据的类
+class CommonViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars': SideBar.get_all(),
+        })
+        context.update(Category.get_navs())
+        return context
 
 
-def post_detail(request, post_id=None):
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        post = None
-    context = {
-        'post': post,
-        'sidebars': SideBar.get_all(),
-    }
-    context.update(Category.get_navs())
-    return render(request, 'blog/detail.html', context=context)
+class IndexView(CommonViewMixin, ListView):
+    queryset = Post.hot_posts()
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
+
+
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+        return context
+
+    def get_queryset(self):
+        """重写queryset,根据分类过滤"""
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag', tag,
+        })
+        return context
+
+    def get_queryset(self):
+        """重写queryset，根据标签过滤"""
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag_id=tag_id)
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+
+# 基于function view的代码
+# def post_list(request, category_id=None, tag_id=None):
+#     tag = None
+#     category = None
+#     #
+#     # if tag_id:
+#     #     try:
+#     #         tag = Tag.objects.get(id=tag_id)
+#     #     except Tag.DoseNotExist:
+#     #         post_list = []
+#     #     else:
+#     #         post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+#     # else:
+#     #     post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+#     #     if category_id:
+#     #         try:
+#     #             category = Category.objects.get(id=category_id)
+#     #         except Category.DoesNotExist:
+#     #             category = None
+#     #         else:
+#     #             post_list = post_list.filter(category_id=category_id)
+#     if tag_id:
+#         post_list, tag = Post.get_by_tag(tag_id)
+#     elif category_id:
+#
+#         post_list, category = Post.get_by_category(category_id)
+#     else:
+#         post_list = Post.latest_posts()
+#
+#     context = {
+#         'tag': tag,
+#         'category': category,
+#         'post_list': post_list,
+#         'sidebars': SideBar.get_all(),
+#     }
+#     context.update(Category.get_navs())
+#
+#     return render(request, 'blog/list.html', context=context)
+
+
+# def post_detail(request, post_id=None):
+#     try:
+#         post = Post.objects.get(id=post_id)
+#     except Post.DoesNotExist:
+#         post = None
+#     context = {
+#         'post': post,
+#         'sidebars': SideBar.get_all(),
+#     }
+#     context.update(Category.get_navs())
+#     return render(request, 'blog/detail.html', context=context)
