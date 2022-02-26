18f20e1779fcdd70cffdd6647ace1336151153b9
FreshManzzzzz <1027118385@qq.com>
2022-02-26 16:08:46 +0800

    8.2 新增bootstrap进行页面样式美化

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -53,11 +53,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'typeidea.urls'
+THEME = 'bootstrap'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'themes', THEME, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
