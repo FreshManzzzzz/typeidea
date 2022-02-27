ee6790ae3e5eb5a563ae422d67d516cc508388eb
FreshManzzzzz <1027118385@qq.com>
2022-02-27 13:53:24 +0800

    9.1 修改了一部分base.html页面的内容 新增根据文章标题以及摘要搜索，新增作者页面

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from blog.models import Post, Tag, Category
 from config.models import SideBar
@@ -70,6 +70,34 @@ class PostDetailView(CommonViewMixin, DetailView):
     template_name = 'blog/detail.html'
     context_object_name = 'post'
     pk_url_kwarg = 'post_id'
+
+
+class SearchView(IndexView):
+    """根据文章标题和摘要搜索文章"""
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context.update({
+            'keyword': self.request.GET.get('keyword', '')
+        })
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        keyword = self.request.GET.get('keyword')
+        if not keyword:
+            return queryset
+        else:
+            return queryset.filter(Q(title__icontains=keyword) | Q(desc__icontains=keyword))
+
+
+class AuthorView(IndexView):
+    """搜索该作者下的所有文章"""
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        author_id = self.kwargs.get('owner_id')
+        return queryset.filter(owner_id=author_id)
 
 # 基于function view的代码
 # def post_list(request, category_id=None, tag_id=None):
