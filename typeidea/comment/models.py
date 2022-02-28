402b42b8095f9c0b14bdc0407920d231496153ae
FreshManzzzzz <1027118385@qq.com>
2022-02-28 18:56:16 +0800

    9.4 增加Markdown

--- typeidea/comment/models.py
+++ typeidea/comment/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from blog.models import Post
 
 
 class Comment(models.Model):
--- typeidea/config/models.py
+++ typeidea/config/models.py
@@ -86,7 +86,7 @@ class SideBar(models.Model):
             result = render_to_string('config/blocks/sidebar_posts.html', context)
         elif self.display_type == self.DISPLAY_COMMENT:
             context = {
-                'comments': Comment.objects.filter(status=Comment.STATUS_NORMAL)
+                'comments': Comment.objects.filter(status=Comment.STATUS_NORMAL).order_by('-created_time')
             }
             result = render_to_string('config/blocks/sidebar_comments.html', context)
         return result
